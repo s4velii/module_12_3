eb610e96cdc32f93b505eea5bae60b965cdc1877
s4velii <44133852+s4velii@users.noreply.github.com>
2024-09-29 21:45:39 +0300

    Add files via upload

---
+++ TestRunner.py
@@ -0,0 +1,43 @@
+from tests_12_4 import Runner
+import unittest
+import logging
+
+
+class TestRunner(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        try:
+            boy = Runner('boy', -5)
+            for _ in range(1, 11):
+                boy.walk()
+            self.assertEqual(boy.distance, 50)
+            logging.info('"test_walk" выполнен успешно')
+        except ValueError:
+            logging.warning(f'Неверная скорость для {Runner.__name__}', exc_info=True)
+            return "0"
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run(self):
+        try:
+            logging.info('"test_run" выполнен успешно')
+            men = Runner(777)
+            for _ in range(1, 11):
+                men.run()
+            self.assertEqual(men.distance, 100)  # FAILED (failures=1) 101 != 100
+        except TypeError:
+            logging.warning('Неверный тип данных для объекта Runner', exc_info=True)
+            return '0'
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_challenge(self):
+        child = Runner('child')
+        runner = Runner('runner')
+        for _ in range(1, 11):
+            child.walk()
+            runner.run()
+        self.assertNotEqual(child.distance, runner.distance)
+
+
+
